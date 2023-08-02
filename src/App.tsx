5922b4874d37b58ce0e6c0e43ebd66dfbb9d6dd0
Kirill Koshcheev <kosheevkirill@gmail.com>
2023-08-03 01:39:33 +0200

    Save before shadcn init

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // import { Product } from '@/features/products/Product'
 import { AdminLayout } from '@/layout/AdminLayout'
 import { ProductCreate } from '@/pages/product/CreateProduct'
+import { ListProducts } from '@/pages/product/ListProducts'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route
-                    path="/product"
+                    path="/products"
+                    element={
+                        <AdminLayout>
+                            <ListProducts />
+                        </AdminLayout>
+                    }
+                />
+                <Route
+                    path="/product/create"
                     element={
                         <AdminLayout>
                             <ProductCreate />
