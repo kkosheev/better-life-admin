6cf6a0c50663c17454ac62c2680c112b724831d3
Kirill Koshcheev <kosheevkirill@gmail.com>
2023-08-04 03:17:09 +0200

    Added Input, Form, Label components

---
+++ src/lib/measurements.ts
@@ -0,0 +1,136 @@
+export const nutrientsLabels = {
+    proximates: {
+        protein: {
+            label: 'Protein',
+            unit: 'g',
+        },
+        water: {
+            label: 'Water',
+            unit: 'g',
+        },
+        energy: {
+            label: 'Energy',
+            unit: 'kcal',
+        },
+        fat: {
+            label: 'Total lipid (fat)',
+            unit: 'g',
+        },
+        carbohydrates: {
+            label: 'Carbohydrates',
+            unit: 'g',
+        },
+        fiber: {
+            label: 'Fiber, total dietary',
+            unit: 'g',
+        },
+        sugars: {
+            label: 'Sugars, Total NLEA',
+            unit: 'g',
+        },
+        salt: {
+            label: 'Salt, NaCl',
+            unit: 'mg',
+        },
+    },
+    minerals: {
+        calcium: {
+            label: 'Calcium, Ca',
+            unit: 'mg',
+        },
+        iron: {
+            label: 'Iron, Fe',
+            unit: 'mg',
+        },
+        magnesium: {
+            label: 'Magnesium, Mg',
+            unit: 'mg',
+        },
+        phosphorus: {
+            label: 'Phosphorus, P',
+            unit: 'mg',
+        },
+        potassium: {
+            label: 'Potassium, K',
+            unit: 'mg',
+        },
+        sodium: {
+            label: 'Sodium, Na',
+            unit: 'mg',
+        },
+        zinc: {
+            label: 'Zinc, Zn',
+            unit: 'mg',
+        },
+        copper: {
+            label: 'Copper, Cu',
+            unit: 'mg',
+        },
+        manganese: {
+            label: 'Manganese, Mn',
+            unit: 'mg',
+        },
+        iodine: {
+            label: 'Iodine, I',
+            unit: 'ug',
+        },
+        selenium: {
+            label: 'Selenium, Se',
+            unit: 'ug',
+        },
+        chlorine: {
+            label: 'Chlorine, Cl',
+            unit: 'mg',
+        },
+        chromium: {
+            label: 'Chromium, Cr',
+            unit: 'ug',
+        },
+    },
+    alcohol: {
+        alcohol: {
+            label: 'Alcohol, ethyl',
+            unit: 'g',
+        },
+    },
+    caffeine: {
+        caffeine: {
+            label: 'Caffeine',
+            unit: 'mg',
+        },
+    },
+}
+
+export const nutrientsTemplate = {
+    proximates: {
+        protein: 0,
+        water: 0,
+        energy: 0,
+        fat: 0,
+        carbohydrates: 0,
+        fiber: 0,
+        sugars: 0,
+        salt: 0,
+    },
+    minerals: {
+        calcium: 0,
+        iron: 0,
+        magnesium: 0,
+        phosphorus: 0,
+        potassium: 0,
+        sodium: 0,
+        zinc: 0,
+        copper: 0,
+        manganese: 0,
+        iodine: 0,
+        selenium: 0,
+        chlorine: 0,
+        chromium: 0,
+    },
+    alcohol: {
+        alcohol: 0,
+    },
+    caffeine: {
+        caffeine: 0,
+    },
+}
--- src/pages/product/CreateProduct/index.tsx
+++ src/pages/product/CreateProduct/index.tsx
@@ -1,5 +1,43 @@
 import React from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { nutrientsTemplate } from '@/lib/measurements'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+    name: z.string().min(2).max(100),
+    image: z.string(),
+    pure: z.boolean(),
+    glycemicIndex: z.number().gte(0).lte(100),
+    nutritionScore: z.number().gte(0).lte(100),
+    quantity: z.string(),
+    price: z.string(),
+})
 
 export const ProductCreate: React.FC = () => {
-    return <div>Create Product</div>
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: '',
+            image: '',
+            pure: true,
+            glycemicIndex: 0,
+            nutritionScore: 0,
+            quantity: 'g',
+            price: '$',
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}></form>
+        </Form>
+    )
 }
